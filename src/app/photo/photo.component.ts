21aa1eba7e90c635fc12948a4a9db596817468af
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-02 14:08:50 -0300

    Inbound properties: passando dados para o componente

--- src/app/photo/photo.component.ts
+++ src/app/photo/photo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-photo',
@@ -6,9 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo.component.css'],
 })
 export class PhotoComponent implements OnInit {
-  url =
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg';
-  description = 'Leão';
+  @Input() url = '';
+  @Input() description = '';
 
   constructor() {}
 
