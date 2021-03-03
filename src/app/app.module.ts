c37e7e4abc7c542406c779c443e652edc65764fc
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-03 11:24:05 -0300

    Componentes de login e registro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
 import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotosModule, ErrorsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    PhotosModule,
+    ErrorsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
