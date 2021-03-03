46f3f838baa29362c8a4583bbf393598a9fa5662
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-03 13:25:16 -0300

    Enviando credenciais para a API

--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { AuthService } from 'src/app/core/auth/auth.service';
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -9,12 +11,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class SigninComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(private formBuilder: FormBuilder, private auth: AuthService) {}
 
   ngOnInit(): void {
     this.form = this.formBuilder.group({
-      username: ['', Validators.required],
+      userName: ['', Validators.required],
       password: ['', Validators.required],
     });
+  }
+
+  signIn() {
+    const userName = this.form.get('userName')?.value;
+    const password = this.form.get('password')?.value;
+
+    this.auth.authenticate(userName, password).subscribe(
+      (response) => {
+        console.log(response);
+      },
+      (err) => {
+        console.log(err);
+        alert('Invalid username or password.');
+        this.form.reset();
+      }
+    );
   }
 }
