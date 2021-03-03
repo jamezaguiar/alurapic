dfcb731af260dc5651bcfbdf52607b5c993dbc5e
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-03 10:05:06 -0300

    Submódulos

---
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PhotoModule } from '../photo/photo.module';
+
+import { PhotoListComponent } from './photo-list.component';
+import { PhotosComponent } from './photos/photos.component';
+import { LoadButtonComponent } from './load-button/load-button.component';
+import { FilterByDescriptionPipe } from './filter-by-description.pipe';
+
+@NgModule({
+  declarations: [
+    PhotoListComponent,
+    PhotosComponent,
+    LoadButtonComponent,
+    FilterByDescriptionPipe,
+  ],
+  imports: [CommonModule, PhotoModule],
+})
+export class PhotoListModule {}
