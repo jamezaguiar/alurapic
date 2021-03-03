801aa30580eebe1b675f4562f78eee773926dbe4
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-03 09:21:59 -0300

    Debounce com RxJS e Subject

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,29 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Photo } from '../photo/photo.model';
-
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css'],
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject();
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.photos = this.route.snapshot.data['photos'];
+
+    this.debounce
+      .pipe(debounceTime(300))
+      .subscribe((filter) => (this.filter = filter));
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();
   }
 
   onKeyUp(target: EventTarget | null) {
     if (target) {
       let elemento = target as HTMLInputElement;
-      this.filter = elemento.value;
+      this.debounce.next(elemento.value);
     }
   }
 }
