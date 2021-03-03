00671d7397f42c6ae8c84523a5ae2b41772e4af5
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-03 11:06:42 -0300

    Diretiva personalizada

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { PhotoModule } from '../photo/photo.module';
 import { CardModule } from '../../shared/components/card/card.module';
+import { DarkenOnHoverModule } from '../../shared/directives/darken-on-hover/darken-on-hover.module';
 
 import { PhotoListComponent } from './photo-list.component';
 import { PhotosComponent } from './photos/photos.component';
@@ -18,6 +19,6 @@ import { SearchComponent } from './search/search.component';
     FilterByDescriptionPipe,
     SearchComponent,
   ],
-  imports: [CommonModule, PhotoModule, CardModule],
+  imports: [CommonModule, PhotoModule, CardModule, DarkenOnHoverModule],
 })
 export class PhotoListModule {}
