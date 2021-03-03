8f55e67b84ffc9427fe0e26eb39a91bda47b6cc2
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-03 11:45:41 -0300

    Validação de formulários

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
-
-
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, ReactiveFormsModule],
 })
-export class HomeModule { }
+export class HomeModule {}
--- src/app/home/signin/signin.component.ts
+++ src/app/home/signin/signin.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
   }
-
 }
