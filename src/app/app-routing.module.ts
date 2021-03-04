9ae415573a23fed73152090d978ea8aa18d13a0f
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-04 09:33:00 -0300

    O componente de registro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,21 @@ import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 import { SigninComponent } from './home/signin/signin.component';
-import { AuthGuard } from './core/auth/auth.guard';
+import { SignupComponent } from './home/signup/signup.component';
 
 import { NotFoundComponent } from './errors/not-found/not-found.component';
+
+import { AuthGuard } from './core/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     component: SigninComponent,
     canActivate: [AuthGuard],
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
   },
   {
     path: 'user/:userName',
