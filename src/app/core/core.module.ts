6068e0eca6e9c76b87485e2e3175d22801a85f54
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-04 09:28:32 -0300

    A diretiva routerLink

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,13 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, RouterModule],
   exports: [HeaderComponent],
 })
 export class CoreModule {
