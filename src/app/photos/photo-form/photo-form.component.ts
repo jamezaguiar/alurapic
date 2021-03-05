205875da783d4e0f2b5a0a1e8285cd17e488d66a
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-05 10:12:37 -0300

    Validando o formulário

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-photo-form',
   templateUrl: './photo-form.component.html',
-  styleUrls: ['./photo-form.component.css']
+  styleUrls: ['./photo-form.component.css'],
 })
 export class PhotoFormComponent implements OnInit {
+  form!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      file: ['', Validators.required],
+      description: ['', Validators.maxLength(300)],
+      allowComments: [true],
+    });
   }
-
 }
