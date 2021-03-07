4a75e2567a1ae64e71ab0e65f618ac38bc70950f
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-07 15:41:38 -0300

    Resolvendo bug na URL da API

--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -3,7 +3,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Photo } from './photo.model';
 
-const API = 'http://localhost:3000/';
+const API = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +12,13 @@ export class PhotoService {
   constructor(private http: HttpClient) {}
 
   listUserPhotos(userName: string) {
-    return this.http.get<Photo[]>(`${API}${userName}/photos`);
+    return this.http.get<Photo[]>(`${API}/${userName}/photos`);
   }
 
   listUserPhotosPaginated(userName: string, page: Number) {
     const params = new HttpParams().append('page', page.toString());
 
-    return this.http.get<Photo[]>(`${API}${userName}/photos`, { params });
+    return this.http.get<Photo[]>(`${API}/${userName}/photos`, { params });
   }
 
   uploadPhoto(description: string, allowComments: boolean, file: File) {
