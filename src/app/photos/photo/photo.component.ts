ffc9e25d29ac79b6115265c6fd1a0d7127e8fe3c
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-07 15:49:03 -0300

    Adequando a exibição das fotos

--- src/app/photos/photo/photo.component.ts
+++ src/app/photos/photo/photo.component.ts
@@ -1,13 +1,26 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+const IMAGES_PATH = 'http://localhost:3000/imgs';
+
 @Component({
   selector: 'app-photo',
   templateUrl: './photo.component.html',
   styleUrls: ['./photo.component.css'],
 })
 export class PhotoComponent implements OnInit {
-  @Input() url = '';
   @Input() description = '';
+
+  private _url = '';
+  @Input() set url(url: string) {
+    if (url.startsWith('data')) {
+      this._url = url;
+    } else {
+      this._url = `${IMAGES_PATH}/${url}`;
+    }
+  }
+  get url() {
+    return this._url;
+  }
 
   constructor() {}
 
