f112821b83217d118e81ab4d64b652e650e71ab9
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-08 10:42:38 -0300

    Validação

--- src/app/photos/photo-details/photo-details.module.ts
+++ src/app/photos/photo-details/photo-details.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { PhotoModule } from '../photo/photo.module';
+import { VmessageModule } from '../../shared/components/vmessage/vmessage.module';
 import { PhotoDetailsComponent } from './photo-details.component';
 import { PhotoCommentsComponent } from './photo-comments/photo-comments.component';
 
 @NgModule({
   declarations: [PhotoDetailsComponent, PhotoCommentsComponent],
-  imports: [CommonModule, RouterModule, PhotoModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
+    PhotoModule,
+    VmessageModule,
+  ],
   exports: [PhotoDetailsComponent, PhotoCommentsComponent],
 })
 export class PhotoDetailsModule {}
