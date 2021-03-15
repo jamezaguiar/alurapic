b69eb0ff9e69ecaa3bdf11d6a52b0341b26ed3d2
Jamerson Aguiar <jamersonalsilva14@gmail.com>
2021-03-15 08:33:58 -0300

    Global Error Handler

--- src/app/errors/errors.module.ts
+++ src/app/errors/errors.module.ts
@@ -1,13 +1,17 @@
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { NotFoundComponent } from './not-found/not-found.component';
-
-
+import { GlobalErrorHandler } from './global-error-handler/global-error-handler';
 
 @NgModule({
   declarations: [NotFoundComponent],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  providers: [
+    {
+      provide: ErrorHandler,
+      useClass: GlobalErrorHandler,
+    },
+  ],
 })
-export class ErrorsModule { }
+export class ErrorsModule {}
